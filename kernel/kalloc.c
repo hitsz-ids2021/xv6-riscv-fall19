261084ecb58f8341f366a91643e993cc6fe58c05
RustLee <48178505+RustLee@users.noreply.github.com>
2020-11-27 17:03:44 +0800

    Update kalloc.c

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -12,7 +12,6 @@
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
 
 struct run {
   struct run *next;
